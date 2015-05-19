8de7d67fbf327a907d0f34af62ac168fd297dbaf
Darrel Herbst <dherbst@gmail.com>
2015-05-19 10:53:48 -0400

    for #4 to fix partial file matches and to fix the min.js.map issue

---
+++ test/fixtures/mapfiles/Gruntfile.js
@@ -0,0 +1,24 @@
+/*
+ * grunt-hashres
+ * https://github.com/DramaFever/grunt-hashres
+ *
+ * Copyright (c) 2015 Darrel Herbst
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    hashres: {
+      mapfiles: {
+        src : ['myscripts.min.js', 'myscripts.min.js.map'],
+        dest: ['*.html', '*.js']
+      }
+    }
+  });
+
+  grunt.loadTasks('../../../tasks');
+
+};
